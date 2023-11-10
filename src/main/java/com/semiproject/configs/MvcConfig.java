65ca445a42039ed1fd2854d0accd17bdb0230c8d
JeongChanSik <ckstlrdlfksp@naver.com>
2023-11-10 14:31:52 +0900

    application.yml 설정값 추가 및 파일 업로드 설정

---
+++ src/main/java/com/semiproject/configs/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.semiproject.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // 설정 객체
+@EnableConfigurationProperties(FileUploadConfig.class)
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileUploadConfig fileUploadConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(fileUploadConfig.getUrl() + "**")
+                .addResourceLocations("file:///" + fileUploadConfig.getPath());
+    }
+}
