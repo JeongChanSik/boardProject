c5b9b39bb10772377eda535081344347e19b7c5e
JeongChanSik <ckstlrdlfksp@naver.com>
2023-11-13 18:17:26 +0900

    로그인 페이지를 벗어날 때 세션 삭제 처리

--- src/main/java/com/semiproject/commons/interceptors/CommonInterceptor.java
+++ src/main/java/com/semiproject/commons/interceptors/CommonInterceptor.java
@@ -1,5 +1,6 @@
 package com.semiproject.commons.interceptors;
 
+import com.semiproject.commons.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -25,5 +26,10 @@ public class CommonInterceptor implements HandlerInterceptor {
         }
         /* PC, Mobile 수동 변경 처리 End */
 
+        /*로그인 페이지가 아닐 경우 로그인 유효성 검사 (세션 삭제 처리) */
+        String URI = request.getRequestURI(); // 주소에 멤버가 포함되어 있는지 확인 여부 체크를 위한 변수
+        if(URI.indexOf("/member/login") == -1) { // 로그인 페이지가 아닐 경우 세션 삭제 쳐리
+            Utils.loginInit(session);
+        }
     }
 }
