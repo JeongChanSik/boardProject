332666cd1ae1d32e69ecc832fd2f44753f143fdc
Jinseong0616 <81826156+Jinseong0616@users.noreply.github.com>
2023-11-15 15:25:03 +0900

    jdbc -> jpa 설정값 변경

--- src/main/java/com/semiproject/entities/Member.java
+++ src/main/java/com/semiproject/entities/Member.java
@@ -1,15 +1,16 @@
 package com.semiproject.entities;
 
 import com.semiproject.commons.constant.MemberType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity /*데이터 하나하나를 의미한다.*/
 public class Member {
-
-    @Id
+    @Id /* JPA에서는 @Id를 반드시 입력해줘야 한다.*/
     private Long userNo;
 
     private String email;
--- src/main/java/com/semiproject/repositories/MemberRepository.java
+++ src/main/java/com/semiproject/repositories/MemberRepository.java
@@ -1,12 +1,11 @@
 package com.semiproject.repositories;
 
 import com.semiproject.entities.Member;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
 }
