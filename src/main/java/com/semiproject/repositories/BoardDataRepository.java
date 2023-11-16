aa2fd401c235c0a115a3af7cc2f71b8f1789eb9a
JeongChanSik <ckstlrdlfksp@naver.com>
2023-11-16 18:20:22 +0900

    QBoard 사용법

--- src/main/java/com/semiproject/repositories/BoardDataRepository.java
+++ src/main/java/com/semiproject/repositories/BoardDataRepository.java
@@ -4,12 +4,14 @@ import com.semiproject.entities.BoardData;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
+// QuerydslPredicateExecutor : 조건식을 상세하게 표현할 수 있다.
+public interface BoardDataRepository extends JpaRepository<BoardData, Long>, QuerydslPredicateExecutor<BoardData> {
 
     List<BoardData> findByCreatedAtBetween(LocalDateTime sdate, LocalDateTime edate, Pageable pageable);
 
